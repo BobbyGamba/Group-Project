7a354ed31d7dc77ab4f2ce79cc625353a08b9402
BobbyGamba <bgamb1025@gmail.com>
2025-03-03 10:32:20 -0700

    Update B_C_Group_Project.c
    Added first couple of story lines

--- B_C_Group_Project.c
+++ B_C_Group_Project.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
 int main() {
+int x;
+ 
+///ASCII art of island
+printf("You awaken to the sounds of waves crashing on the beach\n");
+printf("The water splashes on your face as the waves effortlessly throw you onto the shore\n");
+printf("You stumble too your feet trying to grasp the situation you find youself in\n");
+printf("Before you stands a forest of palm trees towering over you and to your sides a white sand beach that curves to an end you can't see\n");
+printf("Scanning your surroundings you try to wake up from this new terrible nightmare\n");
+printf("1: Yell for help\n2: Look for fellow survivors\n3: Cry\n");
+scanf("%d", &x);
+if (x == 1) { 
+    printf("You try yelling for help, screaming until you can't anymore but your cries fall upon the deaf ears of the trees\n");
+}
+else if (x == 2) {
 
-printf("You awaken to the sounds of waves crashing on the beach");
-test 
+return 0;
+
+}
