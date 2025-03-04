6dacd4a49992584eb8668883f426caf9394d6832
BobbyGamba <bgamb1025@gmail.com>
2025-03-04 11:45:49 -0700

    Update B_C_Group_Project.c
    Added error checking

--- B_C_Group_Project.c
+++ B_C_Group_Project.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
+void clear() {
+    while ( getchar() != '\n' );
+}
+
 int main() {
 int x;
 char y[30];
 int i;
+int rc;
  
 FILE * f=fopen("island.txt","r"); 
     for (i = 0; i <=10; i++) {
@@ -16,8 +21,17 @@ printf("You stumble too your feet trying to grasp the situation you find youself
 printf("Before you stands a forest of palm trees towering over you and to your sides a white sand beach that curves to an end you can't see\n");
 printf("Scanning your surroundings you try to wake up from this new terrible nightmare\n");
 printf("1: Yell for help\n2: Look for fellow survivors\n3: Cry\n");
-scanf("%d", &x);
-if (x == 1) { 
+rc = scanf("%d", &x);
+if (rc != 1) {
+    clear();
+}
+while (x != 1 && x !=2 && x != 3) {
+    printf("Please enter a 1, 2, or 3");
+    rc = scanf("%d", &x);
+    if (rc != 1) {
+        clear();
+    }
+ if (x == 1) { 
     printf("You try yelling for help, screaming until you can't anymore but your cries fall upon the deaf ears of the trees\n");
 }
 else if (x == 2) {
@@ -25,9 +39,6 @@ else if (x == 2) {
 }
 else if (x == 3) {
     printf("Overcome by emotions of grief and despair you fall to your knees and begin to cry. With tears\nrunning down your face you hear the sound of your own wailing echoing through the trees and bouncing back to you.\nThe twisting vines of the trees distorts the sound and it's almost as if the jungle is laughing at you\n");
-}
-else { 
-printf("Please enter a 1, 2, or 3");
 }
 printf("Accepting your new reality you sit back down on the beach when your adrenaline fianlly fades away, and hungry and thirst become your new focus\n");
 printf("These are your hunger and thirst levels. They will start at 100 points and you need to eat and drink to maintain them. If they fall too low\nyou will become weaker and unable to perform as many tasks and if either reach 0 you will die.\n");
