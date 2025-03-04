ec0bd6a49304a05c1d2d4e68ca140254aef291f7
BobbyGamba <bgamb1025@gmail.com>
2025-03-03 21:35:54 -0700

    Update B_C_Group_Project.c
    Finished opening "cinematic"

--- B_C_Group_Project.c
+++ B_C_Group_Project.c
@@ -15,7 +15,16 @@ if (x == 1) {
     printf("You try yelling for help, screaming until you can't anymore but your cries fall upon the deaf ears of the trees\n");
 }
 else if (x == 2) {
+    printf("Frantically, you run up and down the beach, not daring to dive into the thick growth of the jungle,\ntrying to find someone, anyone but to no avail. You return to the spot where you ashed to shore\n");
+}
+else if (x == 3) {
+    printf("Overcome by emotions of grief and despair you fall to your knees and begin to cry. With tears\nrunning down your face you hear the sound of your own wailing echoing through the trees and bouncing back to you.\nThe twisting vines of the trees distorts the sound and it's almost as if the jungle is laughing at you\n");
+}
+else { 
+printf("Please enter a 1, 2, or 3);
+}
 
 return 0;
 
 }
+  
