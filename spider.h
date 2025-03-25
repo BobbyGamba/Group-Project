dc64f909f067b61e3572a5d645d392f8fa2716be
BobbyGamba <bgamb1025@gmail.com>
2025-03-25 11:19:00 -0700

    Update spider.h

--- spider.h
+++ spider.h
@@ -1,3 +1,13 @@
 #include <stdio.h>
 
+int shelter
+
 int spider(
+    if(shelter == 1) {
+
+
+    if(shelter == 0) {
+        printf("In the middle of the night you feel a sharp pinch on your arm.");
+        printf("Too tired to move you swipe at the pain and go back to sleep.");  
+        printf("You wake up in the morning with an aching pain over your whole body unable to move and exhausted.");
+        printf("You're extemely tired and thirsty
