ca1353047b8ba241d2a957aa24f23056064e69fd
dccarpenter329 <dccarpenter329@gmail.com>
2025-03-24 22:51:31 -0700

    Create collectingWood.h
    Header file for collecting wood function

---
+++ collectingWood.h
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "globalVariables.h"
+/*Function for collecting wood*/
+int collectingwood() {
+  int wood, t, coconut=0, difference;
+  char choice1, choice2;
+  printf("You venture along the beach in search of some drift wood.\n");
+  tme(30);
+  sleep(1);
+  printf("You start to find pieces strune across the beach. How many pieces would you like to collect?");
+  scanf("%d", &wood);
+    if (wood>10) {
+      difference= wood-10;
+      printf("You cannot carry that many pieces. You drop %d.\n", difference);
+      wood=10;
+    }
+  printf("You head back.\n");
+  wood_index= wood;
+  t=3*wood+30;
+  tme(t);
+  sleep(2);
+  printf("Would you like to collect more wood? (y or n)\n");
+  scanf(" %c", &choice1);
+    if (choice1=='y') {
+      printf("On your way back you discover a coconut tree with a fallen coconut at its base.\n");
+      sleep(1);
+      printf("Will you collect it? (y or n)");
+      scanf(" %c", &choice2);
+        if (choice2=='y') {
+          coconut=1;
+        }
+      printf("You continue your mission to collect wood.\n");
+      sleep(2);
+      printf("How many pieces of wood will you collect this time?"); 
+      scanf("%d", &wood);
+        if (wood>10) {
+          difference= wood-10;
+          printf("You cannot carry that many pieces. You drop %d.\n", difference);
+          wood=10;
+        }
+      wood_index= wood_index+wood;
+      sleep(3);
+    }
+  return 0;
+}
+
