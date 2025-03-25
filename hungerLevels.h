95ae2ee132ade4126684150500fe2ab3749fd73a
dccarpenter329 <dccarpenter329@gmail.com>
2025-03-24 22:57:10 -0700

    Create hungerLevels.h
    Hunger levels function header file

---
+++ hungerLevels.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/*Hunger levels function*/
+int hunger(int hun) {
+  int h_index=100;
+  h_index=h_index+hun;
+    if (h_index>100) {
+      h_index=100;
+      printf("Health=%d\n", h_index);
+    }
+    else if (h_index<=0) {
+      printf("You died due to hunger.\n");
+    }
+    else {
+      printf("Health=%d\n", h_index);
+    }
+  return 0;
+}
