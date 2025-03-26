ee61c35dda773f9dca81a3e54e55332012aea07d
BobbyGamba <bgamb1025@gmail.com>
2025-03-26 09:39:26 -0700

    Update spider.c

--- spider.c
+++ spider.c
@@ -7,4 +7,4 @@ int spider(int shelter
         printf("Too tired to move you swipe at the pain and go back to sleep.");  
         printf("You wake up in the morning with an aching pain over your whole body unable to move and exhausted.");
         printf("You've been bitten by a spider and are now sick.");
-        
+        h_index = h_index - 25;
