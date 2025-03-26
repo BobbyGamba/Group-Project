ea3272c75d5eb3859b3000e94ddc37820be3644f
BobbyGamba <bgamb1025@gmail.com>
2025-03-26 09:49:08 -0700

    Update spider.c

--- spider.c
+++ spider.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include "index.h"
 
-int spider(int shelter
+int spider(int shelter)
     if(shelter == 0) {
         printf("In the middle of the night you feel a sharp pinch on your arm.");
         printf("Too tired to move you swipe at the pain and go back to sleep.");  
         printf("You wake up in the morning with an aching pain over your whole body unable to move and exhausted.");
         printf("You've been bitten by a spider and are now sick.");
         h_index = h_index - 25;
+    }
+return 0;
+}
