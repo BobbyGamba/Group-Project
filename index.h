b53d5f760325de662023ed5a55bd829db5ae3535
dccarpenter329 <dccarpenter329@gmail.com>
2025-03-30 16:22:22 -0700

    Update index.h

--- index.h
+++ index.h
@@ -1,7 +1,17 @@
-int collectingWood();
-int hungerLevels();
-int thirstLevels();
+#include <stdio.h>
+
+int collectingwood();
+int hunger();
+int thirst();
 int tme();
 int spider();
 int monster();
 int rand_item();
+int shelter;
+int hours;
+int wood_index;
+int blackberries;
+int bananas;
+int water;
+int medicine;
+int hours;
