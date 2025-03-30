a9c56af5af97faef98b09a52deb94035cfcbd316
dccarpenter329 <dccarpenter329@gmail.com>
2025-03-30 16:16:20 -0700

    Update rand_item.c
    Made knife, medicine, and container global variables

--- rand_item.c
+++ rand_item.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include "index.h"
-
+int container=0, medicine=0, knife=0;
 int rand_item() {
   int r;
   srand(time(NULL));
@@ -12,14 +12,17 @@ int rand_item() {
   if(r == 1) {
     printf("As you get closer you realize that a knife has washed ashore from the wreckage. You bend over and pick it up.\n");
     printf("You have acquired a knife.\n");
+    knife++;
   }
   else if(r == 2) {
     printf("As you get closer you realize that a container has washed ashore from the wreckage. You bend over and pick it up.\n");
     printf("You have acquired a container.\n");
+    container++;
   }
   else if(r == 3) {
     printf("As you get closer you realize that a box of medicine has washed ashore from the wreckage. You bend over and pick it up.\n");
     printf("You have acquired a box of medicine.\n");
+    medicine++;
   }
   else if(r == 4) {
     printf("As you get closer you realize that it's only a part of the fragmented remains of the wreckage.\n");
