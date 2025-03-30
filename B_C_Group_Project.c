48cd0aa7c0640828b14164514126e758af750d4c
dccarpenter329 <dccarpenter329@gmail.com>
2025-03-30 14:12:07 -0700

    Update B_C_Group_Project.c

--- B_C_Group_Project.c
+++ B_C_Group_Project.c
@@ -7,7 +7,7 @@ void clr() {
 }
 
 int main() {
-int x, i, rc, shelter = 0, bananas, blackberries;
+int x, i, rc, shelter = 0;
 char y[30], choice1, choice2;
  
 FILE * f=fopen("island.txt","r"); 
@@ -22,30 +22,30 @@ printf("Before you stands a forest of palm trees towering over you and to your s
 printf("Scanning your surroundings you try to wake up from this new terrible nightmare\n");
 printf("1: Yell for help\n2: Look for fellow survivors\n3: Cry\n");
 rc = scanf("%d", &x);
-if (rc != 1) {
+  if (rc != 1) {
     clr();
-}
-while (x != 1 && x !=2 && x != 3) {
+  }
+  while (x != 1 && x !=2 && x != 3) {
     printf("Please enter a 1, 2, or 3");
     rc = scanf("%d", &x);
-    if (rc != 1) {
+      if (rc != 1) {
         clr();
+      }
+  }
+  if (x == 1) { 
+    printf("You try yelling for help, screaming until you can't anymore but your cries fall upon the deaf ears of the trees\n");
+  }
+  else if (x == 2) {
+    printf("Frantically, you run up and down the beach, not daring to dive into the thick growth of the jungle,\ntrying to find someone, anyone but to no avail. You return to the spot where you ashed to shore\n");
+  }
+  else if (x == 3) {
+    printf("Overcome by emotions of grief and despair you fall to your knees and begin to cry. With tears\n");
+    printf("running down your face you hear the sound of your own wailing echoing through the trees and bouncing back to you.\n");
+    printf("The twisting vines of the trees distorts the sound and it's almost as if the jungle is laughing at you\n");
     }
-    if (x == 1) { 
-        printf("You try yelling for help, screaming until you can't anymore but your cries fall upon the deaf ears of the trees\n");
-    }
-    else if (x == 2) {
-        printf("Frantically, you run up and down the beach, not daring to dive into the thick growth of the jungle,\ntrying to find someone, anyone but to no avail. You return to the spot where you ashed to shore\n");
-    }
-    else if (x == 3) {
-        printf("Overcome by emotions of grief and despair you fall to your knees and begin to cry. With tears\nrunning down your face you hear the sound of your own wailing echoing through the trees and bouncing back to you.\nThe twisting vines of the trees distorts the sound and it's almost as if the jungle is laughing at you\n");
-    }
-}
-        clr(rc);
-    }
-printf("Accepting your new reality you sit back down on the beach when your adrenaline fianlly fades away, and hungry and thirst become your new focus\n");
-printf("These are your hunger and thirst levels. They will start at 100 points and you need to eat and drink to maintain them. If they fall too low\nyou will become weaker and unable to perform as many tasks and if either reach 0 you will die.\n");
-
+  printf("Accepting your new reality you sit back down on the beach when your adrenaline fianlly fades away, and hungry and thirst become your new focus\n");
+  printf("These are your hunger and thirst levels. They will start at 100 points and you need to eat and drink to maintain them.\n");
+  printf("If they fall too low you will become weaker and unable to perform as many tasks and if either reach 0 you will die.\n");
   printf("You look off into the horizon and try to guess what time of day it is. With the sun still beating down on you heavily you guess it is around 14:00.\n");
   printf("You only have a few hours before sunset at 19:00.\n");
   sleep(3);
