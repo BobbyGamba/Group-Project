5a64212c2bdf622cf7609deec381dda7c9a14a2b
dccarpenter329 <dccarpenter329@gmail.com>
2025-03-30 16:13:36 -0700

    Update and rename hungerLevels.c to levels.c
    combined hunger and thirst levels

--- levels.c
+++ levels.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "index.h"
+
+/*Thirst levels function*/
+int thirst(int thr) {
+  int t_index=90;
+  t_index=t_index+thr;
+    if (t_index>100) {
+      t_index=100;
+      printf("Thirst Levels: %d\n", t_index);
+    }
+    else if (t_index<=0) {
+      printf("You died due to thirst.\n");
+      printf("Game over.\n");
+    }
+    else {
+      printf("Thirst Levels: %d\n", t_index);
+    }
+  return 0;
+}
 /*Hunger levels function*/
 int hunger(int hun) {
   int h_index=85;
