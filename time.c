e83436ed230a1ab9f8fa99711ba26f9eed2ea1a4
dccarpenter329 <dccarpenter329@gmail.com>
2025-04-01 12:35:58 -0700

    Update time.c

--- time.c
+++ time.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "index.h"
+
 /*Global Variable*/
 int hours=14;
 int index= 0;
@@ -7,22 +8,22 @@ int index= 0;
 /*Time function*/
 int tme(int min) {
   index= index+min;
-  if (index<10) {
-    printf("%d:0%d\n", hours, index);
-  }
-  else if (index<60) {
-    printf("%d:%d\n", hours, index);
-  }
-  else if (index==60) {
-    hours=hours+1;
-    index=0;
-    if (hours>=24) {
-      hours= hours-24;
+    if (index<10) {
+      printf("%d:0%d\n", hours, index);
     }
-    printf("%d:00\n", hours);
-  }
-  else if (index>60) {
-    while (index>=60) {
+    else if (index<60) {
+      printf("%d:%d\n", hours, index);
+    }
+    else if (index==60) {
+      hours=hours+1;
+      index=0;
+        if (hours>=24) {
+          hours= hours-24;
+        }
+      printf("%d:00\n", hours);
+    }
+    else if (index>60) {
+      while (index>=60) {
         index=index-60;
         hours= hours+1;
       }
@@ -30,11 +31,11 @@ int tme(int min) {
         hours= hours-24;
       }
       if (index>=0 && index<10) {
-      printf("%d:0%d\n", hours, index);
-    }
-    else {
-    printf("%d:%d\n", hours, index);
-    }
+        printf("%d:0%d\n", hours, index);
+      }
+      else {
+        printf("%d:%d\n", hours, index);
+      }
     }
     else {
       printf("%d:%d\n", hours, index);
