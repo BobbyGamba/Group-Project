28ee07cb34a679d8d580a941829a3d8172ea322f
BobbyGamba <bgamb1025@gmail.com>
2025-04-01 04:34:03 -0700

    Update monster.c

--- monster.c
+++ monster.c
@@ -2,16 +2,16 @@
 #include <stdlib.h>
 #include "index.h"
 
-int monster(int shelter) {
+int monster() {
     if(shelter == 1) {
-        printf("In the middle of the night you're awoken by the sound of footsteps outside your shelter.");
-        printf("The sounds approach you in the darkness but retreat into the night, you're safe for now...");
+        printf("In the middle of the night you're awoken by the sound of footsteps outside your shelter.\n");
+        printf("The sounds approach you in the darkness but retreat into the night, you're safe for now...\n");
     }
-    if(shelter == 0) {
-        printf("In the middle of the night you're awoken by the sound of footsteps approaching you in the darkness."); 
-        printf("As your eyes try to adjust to peer into the night the bushes in front of you start to rustle.");
-        printf("Before you can begin to think about what's happening a ghastly monster leaps towards you.");
-        printf("The last thing you see is the gleem of the moonlight on the beasts teeth before you are devoured.");
+    else if(shelter == 0) {
+        printf("In the middle of the night you're awoken by the sound of footsteps approaching you in the darkness.\n"); 
+        printf("As your eyes try to adjust to peer into the night the bushes in front of you start to rustle.\n");
+        printf("Before you can begin to think about what's happening a ghastly monster leaps towards you.\n");
+        printf("The last thing you see is the gleem of the moonlight on the beasts teeth before you are devoured.\n");
         printf("GAME OVER");
         exit(0);
     }
