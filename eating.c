8675451d7a7a05c0bb2ac54da602eafaf33ad015
BobbyGamba <bgamb1025@gmail.com>
2025-04-01 04:32:37 -0700

    Update eating.c

--- eating.c
+++ eating.c
@@ -1,107 +1,148 @@
 #include <stdio.h>
+#include "index.h"
 
-int eating(){
-  int ans, ans2, rc;
+int eating() {
+    int ans, ans2, rc, h, t;  
 
-  hunger();
-  thirst();
     printf("Would you like to eat and/or drink? (1. Yes or 2. No) \n");
     rc = scanf("%d", &ans);
     if (rc != 1) {
         clr();
     }
-    while (ans != 1 && ans !=2) {
-        printf("Please enter a 1 or 2");
+
+    while (ans != 1 && ans != 2) {
+        printf("Please enter a 1 or 2: ");
         rc = scanf("%d", &ans);
         if (rc != 1) {
             clr();
         }
     }
-    if(ans == 1) {
-        printf("What would you like? (1.Bananas 2.Coconuts 3. Meat 4.Water)");
+
+    while (ans == 1) {
+        printf("What would you like? (1. Bananas 2. Coconuts 3. Meat 4. Water): ");
         rc = scanf("%d", &ans2);
+        if (rc != 1) {
+            clr();
+        }
+
+        while (ans2 < 1 || ans2 > 4) {
+            printf("Please enter a valid choice (1, 2, 3, or 4): ");
+            rc = scanf("%d", &ans2);
             if (rc != 1) {
                 clr();
             }
-        while (ans2 != 1 && ans2 !=2 && ans2 != 3) {
-            printf("Please enter a 1, 2, or 3");
-            rc = scanf("%d", &ans2);
+        }
+        if (ans2 == 1) {  
+          if (banana_index==0) {
+            printf("You have no bananas");
+          }
+          else {
+          printf("How many would you like to eat? ");
+            rc = scanf("%d", &ans);
+            if (rc != 1) {
+                clr();
+            }
+
+            while (ans > banana_index || rc != 1) {
+                printf("Please enter a valid number no greater than your banana supply: ");
+                rc = scanf("%d", &ans);
                 if (rc != 1) {
                     clr();
-                }   
-            }
-        if(ans2 == 1) {
-          printf("How many would you like to eat?");
-          rc= scanf("%d", &ans);
-            if (rc != 1) {
-              clr();
-            }
-            while (ans> banana_index || rc != 1) {
-              printf("Please enter a valid number no greater than your banana supply.");
-              rc = scanf("%d", &ans);
-                if (rc != 1) {
-                  clr();
                 }
             }
-          banana_index= banana_index-ans;
-          h= ans*10;
-          hunger(h); 
+
+            banana_index -= ans;
+            h = ans * 20;
+            hunger(h);
+        } 
         }
-        else if (ans2 == 2) {
-        printf("How many would you like to drink?");
-          rc= scanf("%d", &ans);
+        else if (ans2 == 2) {  
+        if (coconut_index==0) {
+            printf("You have no coconuts");
+          }
+           else{
+            printf("How many would you like to drink? ");
+            rc = scanf("%d", &ans);
             if (rc != 1) {
-              clr();
+                clr();
             }
-            while (ans> coconut_index || rc != 1) {
-              printf("Please enter a valid number no greater than your coconut supply.");
-              rc = scanf("%d", &ans);
+
+            while (ans > coconut_index || rc != 1) {
+                printf("Please enter a valid number no greater than your coconut supply: ");
+                rc = scanf("%d", &ans);
                 if (rc != 1) {
-                  clr();
+                    clr();
                 }
             }
-          coconut_index= coconut_index-ans;
-          t= ans*10;
-          thirst(t); 
+
+            coconut_index -= ans;
+            t = ans * 15;
+            thirst(t);
+        } 
         }
-        else if (ans2 == 3) {
-         printf("How many would you like to eat?");
-          rc= scanf("%d", &ans);
+        else if (ans2 == 3) {  
+        if (meat_index==0) {
+            printf("You have no meat");
+          }
+            else {printf("How many would you like to eat? ");
+            rc = scanf("%d", &ans);
             if (rc != 1) {
-              clr();
+                clr();
             }
-            while (ans> meat_index || rc != 1) {
-              printf("Please enter a valid number no greater than your meat supply.");
-              rc = scanf("%d", &ans);
+
+            while (ans > meat_index || rc != 1) {
+                printf("Please enter a valid number no greater than your meat supply: ");
+                rc = scanf("%d", &ans);
                 if (rc != 1) {
-                  clr();
+                    clr();
                 }
             }
-          meat_index= meat_index-ans;
-          h= ans*25;
-          hunger(h); 
+
+            meat_index -= ans;
+            h = ans * 30;
+            hunger(h);
+        } 
         }
-        else if (ans2==4) {
-          printf("How many would you like to drink?");
-          rc= scanf("%d", &ans);
+        else if (ans2 == 4) { 
+        if (water_index==0) {
+            printf("You have no water");
+          } 
+            else {
+            printf("How many would you like to drink? ");
+            rc = scanf("%d", &ans);
             if (rc != 1) {
-              clr();
+                clr();
             }
-            while (ans> water_index || rc != 1) {
-              printf("Please enter a valid number no greater than your water supply.");
-              rc = scanf("%d", &ans);
+
+            while (ans > water_index || rc != 1) {
+                printf("Please enter a valid number no greater than your water supply: ");
+                rc = scanf("%d", &ans);
                 if (rc != 1) {
-                  clr();
+                    clr();
                 }
             }
-          water_index= water_index-ans;
-          t= ans*15;
-          thirst(t); 
+
+            water_index -= ans;
+            t = ans * 20;
+            thirst(t);
         }
-        
-    else {
-        printf("You move on.");
+        }
+        printf("Would you like to eat and/or drink again? (1. Yes or 2. No): ");
+        rc = scanf("%d", &ans);
+        if (rc != 1) {
+            clr();
+        }
+
+        while (ans != 1 && ans != 2) {
+            printf("Please enter a 1 or 2: ");
+            rc = scanf("%d", &ans);
+            if (rc != 1) {
+                clr();
+            }
+        }
     }
 
-return 0;
+    printf("You move on.\n");
+    return 0;  
 }
+
