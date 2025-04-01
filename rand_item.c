423486f3fb839f3380d51b58e8e6865f4b36d511
dccarpenter329 <dccarpenter329@gmail.com>
2025-04-01 12:30:08 -0700

    Update rand_item.c

--- rand_item.c
+++ rand_item.c
@@ -2,31 +2,32 @@
 #include <stdlib.h>
 #include <time.h>
 #include "index.h"
+
+/*Global Variables*/
 int container=0, medicine=0, knife=0;
+
 int rand_item() {
   int r;
   srand(time(NULL));
   r = rand()%4+1;
- 
   printf("While walking across the beach you see something wash ashore in the surf.\n");
-  if(r == 1) {
-    printf("As you get closer you realize that a knife has washed ashore from the wreckage. You bend over and pick it up.\n");
-    printf("You have acquired a knife.\n");
-    knife++;
-  }
-  else if(r == 2) {
-    printf("As you get closer you realize that a container has washed ashore from the wreckage. You bend over and pick it up.\n");
-    printf("You have acquired a container.\n");
-    container++;
-  }
-  else if(r == 3) {
-    printf("As you get closer you realize that a box of medicine has washed ashore from the wreckage. You bend over and pick it up.\n");
-    printf("You have acquired a box of medicine.\n");
-    medicine++;
-  }
-  else if(r == 4) {
-    printf("As you get closer you realize that it's only a part of the fragmented remains of the wreckage.\n");
-  }
-
-return 0;
+    if(r == 1) {
+      printf("As you get closer you realize that a knife has washed ashore from the wreckage. You bend over and pick it up.\n");
+      printf("You have acquired a knife.\n");
+      knife++;
+    }
+    else if(r == 2) {
+      printf("As you get closer you realize that a container has washed ashore from the wreckage. You bend over and pick it up.\n");
+      printf("You have acquired a container.\n");
+      container++;
+    }
+    else if(r == 3) {
+      printf("As you get closer you realize that a box of medicine has washed ashore from the wreckage. You bend over and pick it up.\n");
+      printf("You have acquired a box of medicine.\n");
+      medicine++;
+    }
+    else if(r == 4) {
+      printf("As you get closer you realize that it's only a part of the fragmented remains of the wreckage.\n");
+    }
+  return 0;
 }   
