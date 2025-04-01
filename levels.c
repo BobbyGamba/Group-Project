3b6bb9a07c9d76a61369913d6e247b100a50bb39
BobbyGamba <bgamb1025@gmail.com>
2025-04-01 04:33:36 -0700

    Update levels.c

--- levels.c
+++ levels.c
@@ -1,39 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "index.h"
+/*Global Variables*/
+ int t_index=100;
+ int h_index=100;
 
 /*Thirst levels function*/
 int thirst(int thr) {
-  int t_index=90;
   t_index=t_index+thr;
     if (t_index>100) {
       t_index=100;
-      printf("Thirst Levels: %d\n", t_index);
+      printf("Thirst: %d\n", t_index);
     }
     else if (t_index<=0) {
       printf("You died due to thirst.\n");
       printf("Game over.\n");
+      exit(0);
     }
     else {
-      printf("Thirst Levels: %d\n", t_index);
+      printf("Thirst: %d\n", t_index);
     }
   return 0;
 }
 /*Hunger levels function*/
 int hunger(int hun) {
-  int h_index=85;
   h_index=h_index+hun;
     if (h_index>100) {
       h_index=100;
-      printf("Health=%d\n", h_index);
+      printf("Hunger: %d\n", h_index);
     }
     else if (h_index<=0) {
       printf("You died due to hunger.\n");
       printf("GAME OVER");
-      exit(EXIT_SUCCESS);
+      exit(0);
     }
     else {
-      printf("Health=%d\n", h_index);
+      printf("Hunger: %d\n", h_index);
     }
   return 0;
 }
